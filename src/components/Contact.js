ced196f4812ae460f24ebddb36b15f9c9473f2c3
Bhupendra Yadav <bhupendrayadav@Bhupendras-MacBook-Air.local>
2023-11-01 01:42:54 +0530

    EP-13.2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,13 @@
 const Contact = () => {
-    return (
-      <div>
-        <h1>Contact Us Page</h1>
-      </div>
-    );
-  };
-  export default Contact;
+  return (
+    <div>
+      <h1 className="font-bold text-3xl p-4 m-4 text-center">Contact Us Page</h1>
+      <form className="text-center">
+        <input type="text" placeholder="name" className="border border-black p-2 m-2 rounded-lg" />
+        <input type="text" placeholder="message" className="border border-black p-2 m-2 rounded-lg" />
+        <button className="border border-black p-2 m-2 rounded-lg bg-green-400">Submit</button>
+      </form>
+    </div>
+  );
+};
+export default Contact;
